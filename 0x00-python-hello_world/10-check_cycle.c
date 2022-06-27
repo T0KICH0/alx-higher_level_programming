d8d17548f3636d5857bda2300374dc1262498afc
T0KICH0 <nickhailut@gmail.com>
2022-06-28 01:43:04 +0300

    edit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
+ * check_cycle - check if loop exists in cycle
+ * @list: struct to be checked
  *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if it is, 0 if not
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *turtle = list;
+	listint_t *hare = list;
 
-	if (!list)
-		return (0);
-	while (slow && fast && fast->next)
+	if (list == NULL)
+		return(0);
+	while (turtle != NULL && hare != NULL && hare->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		turtle = turtle->next;
+		hare = (hare->next)->next;
+		if (turtle == hare)
 			return (1);
 	}
 	return (0);
