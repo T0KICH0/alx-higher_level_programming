d19cc7d5b580299e1c6ddc3a219cf9e0828b1ef0
T0KICH0 <nickhailut@gmail.com>
2022-09-18 06:01:41 +0300

    edit

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,3 @@
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-
-
-def testing():
-    test = 1
-    return test
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
