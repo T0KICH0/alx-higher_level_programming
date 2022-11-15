d594254b9219138ce32728ad9fd80b2c05d91963
T0KICH0 <nickhailut@gmail.com>
2022-11-15 12:34:43 +0300

    edit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const axios = require('axios');
-const args = process.argv.slice(process.argv.length - 1);
 
-axios({
-  method: 'GET',
-  url: args[0]
-}).then(res => {
-  console.log('code: '.concat(res.status));
-}).catch(err => console.log('code: '.concat(err.response.status)));
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
